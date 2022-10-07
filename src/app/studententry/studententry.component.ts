4c8da8f30fe0216bf4c98339158b6c7a5327f771
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-07 12:50:37 +0530

    add student

---
+++ src/app/studententry/studententry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studententry',
+  templateUrl: './studententry.component.html',
+  styleUrls: ['./studententry.component.css']
+})
+export class StudententryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
