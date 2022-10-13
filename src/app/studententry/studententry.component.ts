43e09164a45b8b058a197013d48c2843bbfc778c
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-14 03:00:31 +0530

    form page

--- src/app/studententry/studententry.component.ts
+++ src/app/studententry/studententry.component.ts
@@ -9,6 +9,38 @@ export class StudententryComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  rollNo=""
+  college=""
+  parentName=""
+  dob=""
+  admissionNo=""
+  bloodgroup=""
+  address=""
+  mobile=""
+  parentNo=""
+  email=""
+
+  readValues=()=>{
+    let data={
+      "name":this.name,
+      "rollNo":this.rollNo,
+      "college":this.college,
+      "parentName":this.parentName,
+      "dob":this.dob,
+      "admissionNo":this.admissionNo,
+      "bloodgroup":this.bloodgroup,
+      "address":this.address,
+      "mobile":this.mobile,
+      "parentNo":this.parentNo,
+      "email":this.email
+
+    }
+
+    console.log(data)
+  }
+ 
+
   ngOnInit(): void {
   }
 
